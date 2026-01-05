f42d6187e6502c6926811a0361e49af8e9b75895
faridy-mahvish <mahvish.faridy@amiti.in>
2026-01-05 18:12:09 +0530

    fixed reducer file

--- tangy-form-reducer.js
+++ tangy-form-reducer.js
@@ -134,6 +134,11 @@ const tangyFormReducer = function (state = initialState, action) {
             } else if (input.tagName === 'TANGY-UNTIMED-GRID') {
               return Object.assign({}, input, {disabled: true, mode: 'TANGY_UNTIMED_GRID_MODE_DISABLED'})
             } else {
+              if (input.xapiStatement && typeof input.xapiStatement === 'object' && input.value !== '') {
+                  input.xapiStatement.verb = {
+                    id: 'http://adlnet.gov/xapi/verbs/answered',
+                  };
+              }
               return Object.assign({}, input, {disabled: true})
             }
           })
